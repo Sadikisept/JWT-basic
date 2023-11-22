f0f2f0d89aaede7e81844bacc2638480d66c9eae
Sadiki Sept <sadikisept26@gmail.com>
2023-11-22 11:48:43 +0200

    first commit

---
+++ routes/main.js
@@ -0,0 +1,9 @@
+
+
+const login = async (req,res) =>{
+    res.send('Fake Logi /Register/Signup Route')
+}
+
+const dashboard = async (req,res) =>{
+    const luckyNumber = Math.floor(Math.random()*100) 
+}
